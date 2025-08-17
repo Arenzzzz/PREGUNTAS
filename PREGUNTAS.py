a7da8ba4803c754788434fa80c1f5a9fc6525556
Arenzzzz <varenzp024@gmail.com>
2025-08-16 18:24:24 -0600

    COMMIT 2

--- PREGUNTAS.py
+++ PREGUNTAS.py
@@ -1,3 +1,10 @@
 # CURADOR DE PREGINTAS
 # Autor: ARENZ PELÁEZ - 1556425
 
+LISTA = [
+    "¿Qué es Python?",
+    "¿Cómo se instala Python?",
+    "¿Qué es una variable en Python?",
+    "¿Cómo se define una función en Python?",
+    "¿Qué son las listas en Python?"
+]
